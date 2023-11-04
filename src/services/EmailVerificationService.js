0896d5e847a74c8001b0e1eb059a3ffc7865ec6d
rakib2067 <rakib7@hotmail.co.uk>
2023-11-04 22:40:31 +0000

    verification logic

--- src/services/EmailVerificationService.js
+++ src/services/EmailVerificationService.js
@@ -1,9 +1,19 @@
+const crypto = require("crypto");
+
 class EmailVerificationService {
   static async createToken(redisClient, userId) {
     //require bcrypt
-    const tokenKey = `verification`;
-    redisClient.set(tokenKey, verificationToken);
+    const tokenKey = `verification:${userId}`;
+    const verificationToken = generateVerificationToken();
+    redisClient.set(tokenKey, verificationToken, "EX", 3600);
   }
+}
+
+function generateVerificationToken() {
+  // Generate a secure random token of a specified length (e.g., 32 bytes, which will result in a 64-character hex string)
+  const tokenLength = 32;
+  const token = crypto.randomBytes(tokenLength).toString("hex");
+  return token;
 }
 
 module.exports = EmailVerificationService;
